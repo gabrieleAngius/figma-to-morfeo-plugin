5f9d1d015fed408186adea67bf64864b347e1fbc
gabrieleAngius <gabriele.angius@omaha-project.com>
2023-05-05 09:19:11 +0200

    fix(#3): renamed the result of parsing on a test

--- src/_shared/utils/parseName.test.ts
+++ src/_shared/utils/parseName.test.ts
@@ -2,22 +2,22 @@ import { parseName } from './parseName';
 
 describe('parseName', () => {
   it('should return camel case name', () => {
-    const camelized = parseName('Primary Dark');
-    expect(camelized).toBe('primaryDark');
+    const parsed = parseName('Primary Dark');
+    expect(parsed).toBe('primaryDark');
   });
 
   it('should not break if the string is empty', () => {
-    const camelized = parseName('');
-    expect(camelized).toBe('');
+    const parsed = parseName('');
+    expect(parsed).toBe('');
   });
 
   it('should return expected name if contains symbols', () => {
-    const camelized = parseName('primary  #darke$t');
-    expect(camelized).toBe('primary#Darke$t');
+    const parsed = parseName('primary  #darke$t');
+    expect(parsed).toBe('primary#Darke$t');
   });
 
   it('should replace slash with dots', () => {
-    const camelized = parseName('Primary/GreenLight');
-    expect(camelized).toBe('primary.GreenLight');
+    const parsed = parseName('Primary/GreenLight');
+    expect(parsed).toBe('primary.GreenLight');
   });
 });
